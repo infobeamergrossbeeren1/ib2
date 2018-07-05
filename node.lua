ad67590e7a116a6de2cc52707f86a8eaafbc2edb
neoepicurians <lindner@igzev.de>
2018-07-05 21:14:09 +0200

    .

--- node.lua
+++ node.lua
@@ -1,6 +1,6 @@
 gl.setup(1920, 1080)
 
-local font = resource.load_font("default-font.ttf")
+local font = resource.load_font("calibri.ttf")
 
 function node.render()
     font:write(120, 320, "Willkommen am IGZ ", 100, 1,1,1,1)
